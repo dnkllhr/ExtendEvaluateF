bebc5fdf9df80a0fceb0d144584493470418b771
Unknown <lentcody@yahoo.com>
2016-11-14 17:54:39 -0500

    Added mergeRegions()

--- Core/BoardRules/Regions.cpp
+++ Core/BoardRules/Regions.cpp
@@ -7,17 +7,55 @@ public:
     int checkOwner(int tileID, int edge);
 private:
     void createRegion(int tileID, int edge);
-    int countEdgesTillCompletion(int placedTileID);
+    int  countEdgesTillCompletion(int placedTileID);
     void mergeRegions(int placedTileID, int placedEdge, int connectingTileID, int connectingEdge);
     std::unordered_map<std::tuple<int, int>, struct regionSet *> regionTracker;    //Takes (tileID, edge) returns set pointer
     struct meeple ownerMeeples[TOTAL_MEEPLES];
 };
+
+
+void mergeRegions(int placedTileID, int placedEdge, int connectingTileID, int connectingEdge)
+{
+    auto placedSearch = regionTracker.find(std::make_tuple(placedTileID, placedEdge));
+    auto connectingSearch = regionTracker.find(std::make_tuple(connectingTileID, connectingEdge));
+    if(placedSearch != regionTracker.end() && connectingSearch != regionTracker.end())
+    {
+        //Update meeple values
+        (connectingSearch->second)->player1Meeples      += (placedSearch->second)->player1Meeples;
+        (connectingSearch->second)->player2Meeples      += (placedSearch->second)->player2Meeples;
+
+        //Update edgesTillCompletion, no need to increment becuase addConnection() handles
+        (connectingSearch->second)->edgesTillCompletion += (placedSearch->second)->edgesTillCompletion;
+
+        //Take over the linked list.
+        ((connectingSearch->second)->tail)->next         = (placedSearch->second)->head;
+        (connectingSearch->second)->tail                 = (placedSearch->second)->tail;
+
+        //Update Hash entries
+        struct tileNode *iter = (placedSearch->second)->head;
+        while(iter != NULL)
+        {
+            regionTracker[(placedSearch->first)] = (connectingSearch->second); 
+            iter = iter->next;
+        }
+
+        //Bye bye memory leaks.
+        delete(iter);
+        //Get rid of old region.
+        delete((placedSearch->second));
+    }
+}
 
 int Regions::addConnection(int placedTileID, int placedEdge, int connectingTileID, int connectingEdge)
 {
     if(connectingTileID == -1) //Empty Edge
     {
         Regions::createRegion(placedTileID, placedEdge);
+        auto search = regionTracker.find(std::make_tuple(tileID, edge));
+        if(search != regionTracker.end())
+        {
+          //
+        }
     }
     auto search = regionTracker.find(std::make_tuple(tileID, edge));
     if(search != example.end())
@@ -28,6 +66,8 @@ int Regions::addConnection(int placedTileID, int placedEdge, int connectingTileI
         (search->second)->edgesTillCompletion++;
         return 0;
 }
+
+void mergeRegions(int placedTileID, )
 
 int Regions::addMeeple(int playerNumber, int tileID, int edge)
 {
