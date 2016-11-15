3d9ca4e7dcfc36c39b42947d360053f186da9939
Brett Simons <brettsimons@outlook.com>
2016-11-15 18:28:06 -0500

    Added functions to check if a tile placement is valid and if a meeple placement is valid. #10

--- Core/BoardRules/Regions.h
+++ Core/BoardRules/Regions.h
@@ -1,11 +1,16 @@
 #ifndef __REGIONS_H
 #define __REGIONS_H
 
+#include "../Tiles/Tile.h"
 #include <unordered_map>
 
 class Regions
 {
     public:
+        static bool validTilePlacement(const Tile& placed, const Tile * boarderingTiles, int arraySize);
+        /// Determines if a placement of a meeple is valid where placed is the new tile that was just placed and edge index is the edge index in which the meeple was placed.
+        static bool validMeeplePlacement(const Tile& old, unsigned int edgeIndex);
+
         int addConnection(int placedTileID, int placedEdge, int connectingTileID, int connectingEdge);
         int addMeeple(int playerNumber, int tileID, int edge);
         int checkOwner(int tileID, int edge);
