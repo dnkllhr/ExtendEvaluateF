43d4381326cdb1bcceea595a3105c97cf6c63261
hnle04 <nhle231@gmail.com>
2016-11-27 17:38:10 -0500

    included tests for scoreRoad and scoreGrass

--- Testing/Core/BoardRules/Board-Rules-Test.cpp
+++ Testing/Core/BoardRules/Board-Rules-Test.cpp
@@ -1,6 +1,7 @@
 #include "../Core/BoardRules/Regions.h"
 #include "../Core/Board/Board.h"
 #include "../Core/BoardManager/BoardManager.h"
+#include "../Core/BoardRules/Rules.h"
 
 #include "gtest/gtest.h"
 
@@ -248,3 +249,317 @@ TEST(RulesTest, ScoreCastle) {
 	actualScore = scoreCastle(newRegion, false);
 	ASSERT(actualScore == 12);
 }
+
+Test(RulesTest, scoreRoad)
+{
+    //Testing a completed road that forms a perfect square. Starts and ends on the same tile.
+    unsigned int startID = 0;
+    Tile *currentTile;
+    Move *currentMove;
+    Coord *currentCoord;
+    Tile **surroundingTiles;
+
+    // test four 'v' tiles placed so that road is closed square. No prey so should be awarded 4 points.
+    currentTile = &(Tile::CreateTileV(1, &startID, None));
+    //upperRightTile
+    currentTile->setRotation(3);
+    testingTilePlacement(&startID, 72,72, currentTile, surroundingTiles);
+    currentTile->placeTile();
+
+    currentTile = &(Tile::CreateTileV(1, &startID, None));
+    //upperLeftTile
+    currentTile->setRotation(2);
+    testingTilePlacement(&startID, 71, 72, currentTile, surroundingTiles);
+    currentTile->placeTile();
+
+    currentTile = &(Tile::CreateTileV(1, &startID, None);
+    //bottomLeftTile
+    currentTile->setRotation(1);
+    testingTilePlacement(&startID, 71, 71, currentTile, surroundingTiles);
+    currentTile->placeTile();
+
+    currentTile = &(Tile::CreateTileV(1, &startID, None));
+    //bottomRightTile
+    currentTile->setRotation(0);
+    testingTilePlacement(&startID, 72, 71, currentTile, surroundingTiles);
+    currentTile->placeTile();
+
+    unsigned int currentTileID = currentTile->getTileId();
+    shared_ptr<struct regionSet> currentSet = getRegions(currentTileID);
+
+    // not sure what to pass through for bool actuallyScore
+    unsigned int returnScore = scoreRoad(currentSet);
+
+    //completed road = 1 pt per tile
+    unsigned int realScore = 4;
+
+    ASSERT_EQ(realScore, returnScore);
+
+}
+
+TEST(RulesTest, scoreGrassAndRoad)
+{
+    //Testing grass next to completed castle/lake and incomplete den
+    //Testing complete road that starts with den and ends with castle
+    unsigned int startID = 0;
+    Tile *currentTile;
+    Move *currentMove;
+    Coord *currentCoord;
+    Tile **surroundingTiles;
+
+    //this creates a complete castle, so grass should be 5 pts
+    currentTile = &(Tile::CreateTileZ(1, &startID, None));
+    currentTile->setRotation(2);
+    testingTilePlacement(&startId, 72, 71, currentTile, surroundingTiles);
+    currentTile->placeTile();
+
+    currentTile = &(Tile::CreateTileG(1, &startID, None));
+    currentTile->setRotation(1);
+    testingTilePlacement(&startID, 72, 70, currentTile, surroundingTiles);
+    currentTile->placeTile();
+
+    currentTile = &(Tile::CreateTileE(1, &startID, None));
+    currentTile->setRotation(0);
+    testingTilePlacement(&startID, 72, 69, currentTile, surroundingTiles);
+    currentTile->placeTile();
+
+    currentTile = &(Tile::CreateTileA(1, &startID, None));
+    currentTile->setRotation(0);
+    testingTilePlacement(&startID, 72, 72, currentTile, surroundingTiles);
+    currentTile->placeTile();
+
+    unsigned int currentTileID = currentTile->getTileId();
+    shared_ptr<struct regionSet> currentSet = getRegions(currentTileID);
+
+    // get score for Grass in this set of 4 tiles that is placed together
+    unsigned int returnGrassScore = scoreGrass(currentSet, currentTileID, 10);
+   // get score for the completed road  in this set of 4 tiles placed together
+    unsigned int returnRoadScore = scoreRoad(currentSet);
+    unsigned int realGrassScore = 5;
+    unsigned int realRoadScore = 2;
+
+    ASSERT_EQ(realGrassScore, returnGrassScore);
+    ASSERT_EQ(realRoadScore, returnRoadScore);
+}
+
+TEST(RulesTest, scoreMoreGrass)
+{
+    //Testing grass next to completed den and completed lake
+    unsigned int startID = 0;
+    Tile *currentTile;
+    Move *currentMove;
+    Coord *currentCoord;
+    Tile **surroundingTiles;
+
+    currentTile = &(Tile::CreateTileB(1, &startID, None));
+    currentTile->setRotation(0);
+    testingTilePlacement(&startID, 72, 72, currentTile, surroundingTiles);
+    currentTile->placeTile();
+
+    currentTile = &(Tile::CreateTileV(1, &startID, None));
+    currentTile->setRotation(2)
+    testingTilePlacement(&startID, 71, 73), currentTile, surroundingTiles);
+    currentTile->placeTile()
+
+    currentTile = &(Tile::CreateTileU(1, &startID, None));
+    currentTile->setRotation(1);
+    testingTilePlacement(&startID, 72, 73, currentTile, surroundingTiles);
+    currentTile->placeTile();
+
+    currentTile = &(Tile::CreateTileV(1, &startID, None));
+    currentTile->setRotation(3);
+    testingTilePlacement(&startID, 71, 73), currentTile, surroundingTiles);
+    currentTile->placeTile()
+
+    currentTile = &(Tile::CreateTileW(1, &startID, None));
+    currentTile->setRotation(0);
+    testingTilePlacement(&startID, 71, 72, currentTile, surroundingTiles);
+    currentTile->placeTile();
+
+    currentTile = &(Tile::CreateTileY(1, &startID, None));
+    currentTile->setRotation(0);
+    testingTilePlacement(&startID, 72, 71, currentTile, surroundingTiles);
+    currentTile->placeTile();
+
+    currentTile = &(Tile::CreateTileV(1, &startID, None));
+    currentTile->setRotation(0);
+    testingTilePlacement(&startId, 71, 71, currentTile, surroundingTiles);
+    currentTile->placeTile();
+
+    currentTile = &(Tile::CreateTileE(1, &startID, None));
+    currentTile->setRotation(0);
+    testingTilePlacement(&startID, 73, 71);
+    currentTile->placeTile();
+
+    currentTile = &(Tile::CreateTileZ(1, &startID, None));
+    currentTile->setRotation(2);
+    testingTilePlacement(&startID, 73, 72, currentTile, surroundingTiles);
+    currentTile->placeTile();
+
+
+    unsigned int currentTileID = currentTile->getTileId();
+    shared_ptr<struct regionSets> currentSet = getRegions(currentTileID);
+
+    unsigned int returnGrassScore = scoreGrass(curentSet, currentTileID, 10);
+    int realGrassScore = 8; //5 pt for completed lake + 3 pts for completed den
+
+    ASSERT_EQ(returnGrassScore, realGrassScore);
+}
+
+TEST(RulesTest, scoreMoreRoads)
+{
+    //Testing incomplete road with prey animals
+    unsigned int startID = 0;
+    Tile *currentTile;
+    Move *currentMove;
+    Coord *currentCoord;
+    Tile **surroundingTiles;
+
+    currentTile = &(Tile::CreateTileZ(1, &startID, Deer));
+    currentTile->setRotation(3);
+    testingTilePlacement(&startID, 72, 72, currentTile, surroundingTiles);
+    currentTile->placeTile();
+
+    currentTile = &(Tile::CreateTileK(1, &startID, Boar));
+    currentTile->setRotation(0);
+    testingTilePlacement(&startID, 73, 72, currentTile, surroundingTiles);
+    currentTile->placeTile();
+
+    currentTile = &(Tile:CreateTileU(1, &startID, None));
+    currentTile->setRotation(1);
+    testingTilePlacement(&tartID, 74, 73, currentTile, surroundingTiles);
+    currentTile->placeTile();
+
+    currentTile = &(Tile::CreateTileJ(1, &startID, Buffalo));
+    currentTile->setRotation(3);
+    testingTilePlacement(&startID, 73, 73, currentTile, surroundingTiles);
+    currentTile->placeTile();
+
+    unsigned int currentTileID = currentTile->getTileId();
+
+    shared_ptr<struct regionSets> currentSet = getRegions(currentTileID);
+
+    unsigned int returnRoadScore = scoreRoad(currentSet);
+
+    unsigned int realRoadScore = 7;
+
+    ASSERT_EQ(realRoadScore, returnRoadScore);
+}
+
+TEST (RulesTest, scoreMoreMoreRoads)
+{
+    //Testing complete road that starts with den and ends with den and has prey animals
+    unsigned int startID = 0;
+    Tile *currentTile;
+    Move *currentMove;
+    Coord *currentCoord;
+    Tile **surroundingTiles;
+
+    currentTile = &(Tile::CreateTileA(1, &startID, None));
+    currentTile->setRotation(3);
+    testingTilePlacement(&startID, 72, 72, currentTile, surroundingTiles);
+    currentTile->placeTile();
+
+    currentTile = &(Tile::CreateTileJ(1, &startID, Buffalo));
+    currentTile->setRotation(0);
+    testingTilePlacement(&startID, 73, 72, currentTile, surroundingTiles);
+    currentTile->placeTile();
+
+    currentTile = &(Tile::CreateTileV(1, &startID, None));
+    currentTile->setRotation(1);
+    testingTilePlacement(&startID, 73, 71, currentTile, surroundingTiles);
+    currentTile->placeTile();
+
+    currentTile = &(Tile::CreateTileA(1, &startID, None));
+    currentTile->setRotation(1);
+    testingTilePlacement(&startID, 72, 72, currentTile, surroundingTiles);
+    currentTile->placeTile();
+
+    unsigned int currentTileID = currentTile->getTileId();
+
+    shared_ptr<struct regionSets> currentSet = getRegions(currentTileID);
+
+    unsigned int returnRoadScore = scoreRoad(currentSet);
+
+    unsigned int realRoadScore = 5;
+
+    ASSERT_EQ(realRoadScore, returnRoadScore);
+}
+
+TEST(RulesTest, scoreGrassWithJustCompleteDen)
+{
+    //Testing grass next to completed den but incomplete lake
+    //Testing two different incomplete sets of roads
+    unsigned int startID = 0;
+    Tile *currentTile;
+    Move *currentMove;
+    Coord *currentCoord;
+    Tile **surroundingTiles;
+
+    currentTile = &(Tile::CreateTileJ(1, &startID, Buffalo));
+    currentTile->setRotation(3);
+    testingTilePlacement(&startID, 72, 72, currentTile, surroundingTiles);
+    currentTile->placeTile();
+
+    currentTile = &(Tile::CreateTileV(1, &startID, None));
+    currentTile->setRotation(0);
+    testingTilePlacement(&startID, 73,72, currentTile, surroundingTiles);
+    currentTile->placeTile();
+
+    currentTile = &(Tile::CreateTileA(1, &startID, None));
+    currentTile->setRotation(1);
+    testingTilePlacement(&startID, 72, 72, currentTile, surroundingTiles);
+    currentTile->placeTile();
+
+    currentTile = &(Tile::CreateTileU(1, &startID, None));
+    currentTile->setRotation(0);
+    testingTilePlacement(&startID, 73,70, currentTile, surroundingTiles);
+    currentTile->placeTile();
+
+    currentTile = &(Tile::CreateTileU(1, &startID, None));
+    currentTile->setRotation(0);
+    testingTilePlacement(&startID, 72,70, currentTile, surroundingTiles);
+    currentTile->placeTile();
+
+    currentTile = &(Tile::CreateTileU(1, &startID, None));
+    currentTile->setRotation(1);
+    testingTilePlacement(&startID, 74,72, currentTile, surroundingTiles);
+    currentTile->placeTile();
+
+    currentTile = &(Tile::CreateTileV(1, &startID, None));
+    currentTile->setRotation(0);
+    testingTilePlacement(&startID, 74,70, currentTile, surroundingTiles);
+    currentTile->placeTile();
+
+    currentTile = &(Tile::CreateTileD(1, &startId, Deer));
+    currentTile->setRotation(0);
+    testingTilePlacement(&startID, 74,71, currentTile, surroundingTiles);
+    currentTile->placeTile();
+
+    //Testing road portion of this landscape
+    unsigned int currentTileId = currentTile->getTileId();
+    shared_ptr<struct regionSets> currentSet = getRegions(currentTileId);
+
+    unsigned int returnRoadScore = scoreRoad(currentSet);
+
+    unsigned int realRoadScore = 6;
+
+    ASSERT_EQ(realRoadScore, returnRoadScore);
+
+    currentTile = &(Tile::CreateTileV(1, &startID, None));
+    currentTile.setRotation(1);
+    testingTilePlacement(&startID, 72, 71, currentTile, surroundingTiles);
+    currentTile.placeTile();
+
+    unsigned int currentTileID = currentTile->getTileId();
+    shared_ptr<struct regionSets> currentSet2 = getRegions(currentTileID);
+
+    unsigned int return returnRoadScore2 = scoreRoad(currentSet2);
+    unsigned int returnGrassScore = scoreGrass(currentSet2, currentTileID, 7);
+    unsigned int realRoadScore2 = 5;
+    unsigned int realGrassScore  3;
+    ASSERT_EQ(realRoadScore2, returnRoadScore2);
+    ASSERT_EQ(realGrassScore, returnGrassScore);
+
+}
+
