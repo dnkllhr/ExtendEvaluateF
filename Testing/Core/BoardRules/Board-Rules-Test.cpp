f8e061a44abe516c80babbcbf9e08750a7c5d8dd
jacobmellinger <jacobmell@ufl.edu>
2016-11-26 19:39:59 -0500

    Adjusted scoreChurch test

--- Testing/Core/BoardRules/Board-Rules-Test.cpp
+++ Testing/Core/BoardRules/Board-Rules-Test.cpp
@@ -196,7 +196,18 @@ TEST(RulesTest, ScoreChurch) {
 
 				// how should actuallyScore change values returned?
 				actualScore = scoreChurch(tilesSurrounded, true);
-				ASSERT(actualScore == expectedScore);
+
+				// just placed final boardering tile, actuallyScore set to true should return full value
+				if (i == 1 && j == 1)
+				{
+					ASSERT(actualScore == expectedScore)
+				}
+
+				// church isn't completely surrounded, actuallyScore set to true should return 0
+				else
+				{
+					ASSERT(actualScore == 0);
+				}
 				actualScore = scoreChurch(tilesSurrounded, false);
 				ASSERT(actualScore == expectedScore);
 			}
